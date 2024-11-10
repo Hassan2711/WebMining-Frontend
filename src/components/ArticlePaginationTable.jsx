840cdbcb6e3287c80d030ee64adc9bef447eb072
Ali Raza <alirazacs61@gmail.com>
2024-11-10 05:06:42 +0500

    Article factory user check

--- src/components/ArticlePaginationTable.jsx
+++ src/components/ArticlePaginationTable.jsx
@@ -1,299 +1,3 @@
-// "use client";
-// import React, { useState, useEffect } from "react";
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableContainer,
-//   TableHead,
-//   TableRow,
-//   TablePagination,
-//   Paper,
-//   IconButton,
-// } from "@mui/material";
-// import EditIcon from "@mui/icons-material/Edit";
-// import {
-//   Modal,
-//   ModalTrigger,
-//   ModalBody,
-//   ModalContent,
-// } from "@/components/ui/TableModal";
-// import { LabelInputContainer } from "@/components/ui/Login";
-// import { Label } from "@/components/ui/label";
-// import { Input } from "@/components/ui/input";
-
-// const ArticleTablePaginator = ({
-//   data,
-//   totalCount,
-//   page,
-//   rowsPerPage,
-//   onChangePage,
-//   onChangeRowsPerPage,
-// }) => {
-//   const [selectedRow, setSelectedRow] = useState(null);
-//   const [visibleRows, setVisibleRows] = useState([]); // State to manage visible rows
-
-//   // Effect to control the delayed rendering of rows
-//   useEffect(() => {
-//     setVisibleRows([]); // Reset visible rows on data load
-
-//     data.forEach((_, index) => {
-//       setTimeout(() => {
-//         setVisibleRows((prev) => [...prev, index]);
-//       }, index * 300); // Adjust delay time as needed (300ms here)
-//     });
-
-//     return () => {
-//       setVisibleRows([]); // Cleanup timeout when data changes
-//     };
-//   }, [data]);
-
-//   const handleEditClick = (row) => {
-//     setSelectedRow(row); // Set the selected row data
-//   };
-
-//   const handleCloseModal = () => {
-//     setSelectedRow(null); // Clear the selected row data when closing the modal
-//   };
-
-//   const handleChange = (event) => {
-//     setSelectedRow({
-//       ...selectedRow,
-//       [event.target.id]: event.target.value,
-//     });
-//   };
-
-//   if (!data || data.length === 0) {
-//     return (
-//       <Paper className="p-4 text-center">
-//         <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold">
-//           No data available
-//         </h4>
-//         <p>Please check back later</p>
-//       </Paper>
-//     );
-//   }
-
-//   return (
-//     <Paper>
-//       <TableContainer>
-//         <Table>
-//           <TableHead>
-//             <TableRow>
-//                <TableCell>Business Name</TableCell>
-
-//               <TableCell>Status</TableCell>
-//               <TableCell>Checked By</TableCell>
-//               <TableCell>Edit</TableCell>
-//             </TableRow>
-//           </TableHead>
-//           <TableBody>
-//             {data.map((row, index) =>
-//               visibleRows.includes(index) ? (
-//                 <TableRow key={row.id}>
-//                   <TableCell>
-//                     {row.name ? row.name : row.title ? row.title : "N/A"}
-//                   </TableCell>
-
-//                   <TableCell>
-//                     <div className="flex space-x-4 items-center">
-//                       <p>Approved</p>
-//                       <p>
-//                         <svg
-//                           xmlns="http://www.w3.org/2000/svg"
-//                           width={24}
-//                           height={24}
-//                           viewBox="0 0 24 24"
-//                           fill="none"
-//                           stroke="currentColor"
-//                           strokeWidth={2}
-//                           strokeLinecap="round"
-//                           strokeLinejoin="round"
-//                           className="lucide lucide-circle-check bg-green-500 rounded-full text-white"
-//                         >
-//                           <circle cx={12} cy={12} r={10} />
-//                           <path d="m9 12 2 2 4-4" />
-//                         </svg>
-//                       </p>
-//                     </div>
-//                   </TableCell>
-//                    <TableCell align="center" className="capitalize text-center font-semibold mx-auto">{getUser || "Jared"}</TableCell>
-//                   <TableCell>
-//                     <IconButton
-//                       aria-label="edit"
-//                       onClick={() => handleEditClick(row)}
-//                     >
-//                       <Modal>
-//                         <ModalTrigger>
-//                           <EditIcon />
-//                         </ModalTrigger>
-
-//                         <ModalBody onClose={handleCloseModal}>
-//                           <ModalContent>
-//                             <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-2">
-//                               Edit Here
-//                             </h4>
-//                             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-2">
-//                               <LabelInputContainer className="mb-4">
-//                                 <Label htmlFor="businessName">
-//                                   Business Name
-//                                 </Label>
-//                                 <Input
-//                                   id="name"
-//                                   value={
-//                                     selectedRow?.name ||
-//                                     selectedRow?.title ||
-//                                     ""
-//                                   }
-//                                   onChange={handleChange}
-//                                   placeholder={
-//                                     selectedRow?.name
-//                                       ? "Alpha LLC"
-//                                       : selectedRow?.title
-//                                       ? "Title Placeholder"
-//                                       : "Alpha LLC"
-//                                   }
-//                                   type="text"
-//                                 />
-//                               </LabelInputContainer>
-
-//                               <LabelInputContainer className="mb-4">
-//                                 <Label htmlFor="phoneNumber">Article Url</Label>
-//                                 <Input
-//                                   id="phone"
-//                                   value={selectedRow?.article_url || ""}
-//                                   onChange={handleChange}
-//                                   placeholder="541-654-6168"
-//                                   type="tel"
-//                                 />
-//                               </LabelInputContainer>
-
-//                               <LabelInputContainer className="mb-4">
-//                                 <Label htmlFor="address">Author</Label>
-//                                 <Input
-//                                   id="address"
-//                                   value={selectedRow?.author || ""}
-//                                   onChange={handleChange}
-//                                   placeholder="123 Main St, London, UK"
-//                                   type="text"
-//                                 />
-//                               </LabelInputContainer>
-
-//                               <LabelInputContainer className="mb-4">
-//                                 <Label htmlFor="website">Categories</Label>
-//                                 <Input
-//                                   id="website"
-//                                   value={selectedRow?.categories || ""}
-//                                   onChange={handleChange}
-//                                   placeholder="https:www.alpha-llc.com"
-//                                   type="url"
-//                                 />
-//                               </LabelInputContainer>
-
-//                               <LabelInputContainer className="mb-4">
-//                                 <Label htmlFor="moreInfo">Date Time</Label>
-//                                 <Input
-//                                   id="moreInfo"
-//                                   value={selectedRow?.date_time || ""}
-//                                   onChange={handleChange}
-//                                   placeholder="Additional details about the business"
-//                                   type="text"
-//                                 />
-//                               </LabelInputContainer>
-
-//                               <LabelInputContainer className="mb-4">
-//                                 <Label htmlFor="extraPhones">Media Image</Label>
-//                                 <Input
-//                                   id="extraPhones"
-//                                   value={selectedRow?.media_image || ""}
-//                                   onChange={handleChange}
-//                                   placeholder="041-564-5612"
-//                                   type="tel"
-//                                 />
-//                               </LabelInputContainer>
-
-//                               <LabelInputContainer className="mb-4 md:col-span-3">
-//                                 <Label htmlFor="otherLink">Tags</Label>
-//                                 <Input
-//                                   id="otherLink"
-//                                   value={selectedRow?.tags || ""}
-//                                   onChange={handleChange}
-//                                   placeholder="http:www.example.com"
-//                                   type="url"
-//                                 />
-//                               </LabelInputContainer>
-//                               {/*
-//                               <LabelInputContainer className="mb-4">
-//                                 <Label htmlFor="cuisines">Cuisines</Label>
-//                                 <Input
-//                                   id="cuisines"
-//                                   value={selectedRow?.cuisines || ""}
-//                                   onChange={handleChange}
-//                                   placeholder="Bed & Breakfast, Resorts, Guesthouses"
-//                                   type="text"
-//                                 />
-//                               </LabelInputContainer>
-
-//                               <LabelInputContainer className="mb-4">
-//                                 <Label htmlFor="shortDetails">
-//                                   Restaurant Short Description
-//                                 </Label>
-//                                 <Input
-//                                   id="shortDetails"
-//                                   value={selectedRow?.short_details || ""}
-//                                   onChange={handleChange}
-//                                   placeholder="A brief summary of the restaurant."
-//                                   type="text"
-//                                 />
-//                               </LabelInputContainer>
-//                               <LabelInputContainer className="mb-4 md:col-span-3">
-//                                 <Label htmlFor="longDetails">
-//                                   Restaurant Long Description
-//                                 </Label>
-//                                 <textarea
-//                                   id="longDetails"
-//                                   value={selectedRow?.long_details || ""}
-//                                   onChange={handleChange}
-//                                   placeholder="A detailed description of the restaurant's offerings and atmosphere."
-//                                   rows={6}
-//                                   className="w-full h-40 p-3 text-lg  rounded-md border border-black"
-//                                 />
-//                               </LabelInputContainer> */}
-//                             </div>
-//                             <div className="flex space-x-4 items-center justify-center ">
-//                               <button className="px-8 py-2 rounded-md bg-red-500 text-white font-bold transition duration-200 hover:bg-white hover:text-red-500 border-2 border-transparent hover:border-red-500">
-//                                 Reject
-//                               </button>
-//                               <button className="px-8 py-2 rounded-md bg-teal-500 text-white font-bold transition duration-200 hover:bg-white hover:text-teal-500 border-2 border-transparent hover:border-teal-500">
-//                                 Approved
-//                               </button>
-//                             </div>
-//                           </ModalContent>
-//                         </ModalBody>
-//                       </Modal>
-//                     </IconButton>
-//                   </TableCell>
-//                 </TableRow>
-//               ) : null
-//             )}
-//           </TableBody>
-//         </Table>
-//       </TableContainer>
-//       <TablePagination
-//         rowsPerPageOptions={[10, 25, 50, 250, 500, 1000]}
-//         component="div"
-//         count={totalCount}
-//         rowsPerPage={rowsPerPage}
-//         page={page}
-//         onPageChange={onChangePage}
-//         onRowsPerPageChange={onChangeRowsPerPage}
-//       />
-//     </Paper>
-//   );
-// };
-
-// export default ArticleTablePaginator;
-
 "use client";
 import React, { useState, useEffect } from "react";
 import {
@@ -357,6 +61,7 @@ const ArticleTablePaginator = ({
   const [mediaImage, setMediaImage] = useState("");
   const [tags, setTags] = useState("");
   const [content, setContent] = useState("");
+  const [checkedByUser, setCheckedByUser] = useState('N/A');
 
   // const submitData = async (id) => {
   //   try {
@@ -475,6 +180,24 @@ const ArticleTablePaginator = ({
       setContent(selectedRow.article_content || "");
     }
   }, [selectedRow]);
+
+  useEffect(() => {
+    const fetchCheckedByData = async () => {
+      try {
+        const response = await fetch(`${BACKEND_URL}/checkedby`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch checked by data');
+        }
+        const data = await response.json();
+        setCheckedByUser(data.article_factory || 'N/A'); // Note: using article_factory instead of yellowpages
+      } catch (error) {
+        console.error('Error fetching checked by data:', error);
+        setCheckedByUser('N/A');
+      }
+    };
+
+    fetchCheckedByData();
+  }, []);
 
   const handleEditClick = (row) => {
     setSelectedRow(row); // Set the selected row data
@@ -602,7 +325,7 @@ const ArticleTablePaginator = ({
                     align="center"
                     className="capitalize text-center font-semibold mx-auto"
                   >
-                    {getUser || "Jared"}
+                    {checkedByUser}
                   </TableCell>
                   <TableCell>
                     <div onClick={() => handleEditClick(row)}>
